db5c266b75923e00cfbbc8ee97a3467370dd341a
oprilzeng <z.77@qq.com>
2014-03-21 18:01:10 +0800

    fix

--- 2048/full/js/game_manager.js
+++ 2048/full/js/game_manager.js
@@ -145,8 +145,8 @@ GameManager.prototype.move = function (direction) {
           if (merged.value > maxscore)
             maxscore = merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          // The mighty 131072 tile
+          if (merged.value === 131072) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
