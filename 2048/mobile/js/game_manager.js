86fd82119a85bb229c603c5bbafc72f319a41fe6
ymll <yml.lily@gmail.com>
2014-03-29 04:12:21 +0800

    ===

--- 2048/mobile/js/game_manager.js
+++ 2048/mobile/js/game_manager.js
@@ -260,7 +260,7 @@ GameManager.prototype.tileMatchesAvailable = function () {
 
           var other  = self.grid.cellContent(cell);
 
-          if (other && other.value === tile.value) {
+          if (other && other.value == tile.value) {
             return true; // These two tiles can be merged
           }
         }
